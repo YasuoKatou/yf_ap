baa8ad683fd9c1b5d287ce1bc7a42c4fc745b5f8
yasuo katou <yasuokatou@gmail.com>
2026-08-14 08:45:58 +0900

    コミット漏れ

--- ap_common.py
+++ ap_common.py
@@ -1,4 +1,7 @@
 import argparse
+import datetime
+
+import jpholiday
 
 def getArgs(params):
     parser = argparse.ArgumentParser()
@@ -8,5 +11,24 @@ def getArgs(params):
         _default = value['default'] if 'default' in value else None
         parser.add_argument(name, help=_help, default=_default, type=_type)
     return parser.parse_args()
+
+def count_business_days(start_date, end_date):
+    current = start_date
+    business_days = 0
+    while current <= end_date:
+        # 土日（5, 6）でも祝日でもなければカウント
+        if current.weekday() < 5 and not jpholiday.is_holiday(current):
+            business_days += 1
+        current += datetime.timedelta(days=1)
+    return business_days
+
+def _count_business_days():
+    # 使用例
+    start = datetime.date(2025, 7, 10)
+    end = datetime.date(2026, 8, 10)
+    print(count_business_days(start, end))
+
+if __name__ == '__main__':
+    _count_business_days()
 
 #[EOF]
--- check_db.py
+++ check_db.py
@@ -1,8 +1,12 @@
+import datetime
 import sqlite3
 import pathlib
 
-db_path_default = './data/yfinance.db.20260804'
+from ap_common import count_business_days
+
+db_path_default = './data/yfinance.db.check'
 tname01 = 'brand'       # 銘柄テーブル名
+tname02_pfx = 'hist_'   # 履歴テーブル接頭句
 def check_01(db_path=db_path_default):
     '''
     sqlite dc のパスを確認する
@@ -31,18 +35,63 @@ def get_brand_info(cur):
     for row in cur:
         yield (row[0], row[1])
 
+def check_03(cur, tno, brand_code, brand_name):
+    print(f'{tno:5} {brand_code}:{brand_name}')
+    qry = f"select count(1),min(\"index\"), max(\"index\") from {tname02_pfx}{brand_code}"
+    try:
+        cur.execute(qry)
+        row = cur.fetchone()
+        recs = row[0]
+        dt1  = row[1]
+        dt2  = row[2]
+        #print(type(dt2))
+        dt1o  = datetime.datetime.strptime(dt1, "%Y-%m-%d")
+        dt2o  = datetime.datetime.strptime(dt2, "%Y-%m-%d")
+        today = datetime.date.today()
+        pdays = count_business_days(dt2o.date(), today)
+        ldays = count_business_days(dt1o.date(), dt2o.date())
+        print(f'      pass {pdays} days {dt2} - {dt1}, {recs} days lost {ldays-recs} days')
+        return [pdays]
+    except sqlite3.OperationalError as ex:
+        print(f'error : {ex}')
+        return None
+    except Exception as ex:
+        assert False, (f'unexpected error\n{ex}')
+
 if __name__ == '__main__':
     db_path = check_01()
     print(f'db path : {db_path}')
+    pmax = 0
+    pmin = 365 * 100
+    plist = {}
     with sqlite3.connect(db_path) as conn:
         cur1 = conn.cursor()
         cur2 = conn.cursor()
         try:
             check_02(cur1)
+            tno = 0
             for brand in get_brand_info(cur1):
-                print(f'{brand[0]}:{brand[1]}')
+                tno += 1
+                r = check_03(cur2, tno, brand[0], brand[1])
+                if r is None:
+                    continue
+                if r[0] < pmin:
+                    pmin = r[0]
+                if pmax < r[0]:
+                    pmax = r[0]
+                if r[0] in plist:
+                    plist[r[0]] += 1
+                else:
+                    plist[r[0]] = 1
         finally:
             cur1.close()
             cur2.close()
+    print(f'pass : {pmin} - {pmax} days')
+    #print(plist)
+    ##sorted_d = dict(sorted(plist.items()))
+    wk = sorted(plist.items(), key=lambda x:x[0])
+
+    for k, v in wk:
+        print(f'\t{k:4} : {v:5}')
 
 #[EOF]
