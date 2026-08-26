0aecd87cfb32f5a2fc228ad616716fdeeb2ff019
yasuo katou <yasuokatou@gmail.com>
2026-08-26 12:22:55 +0900

    履歴取込の状況を最新のログファイルから表示する

--- check_hist_get_status.sh
+++ check_hist_get_status.sh
@@ -3,6 +3,6 @@
 log_folder=logs
 log_pattern=update_hist_*.log
 log_file=$(ls -t ./$log_folder/$log_pattern | head -n 1)
-#echo $log_file
-grep records $log_file | wc -l
+num=$(grep records $log_file | wc -l)
+echo "$log_file : $num"
 
