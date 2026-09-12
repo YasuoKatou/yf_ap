a9acfd8042a471da3abb128f62cfe6dcde914058
yasuo katou <yasuokatou@gmail.com>
2026-09-12 15:40:51 +0900

    優良銘柄を判定する閾値をシェルの引数で変更可能にした

--- ana1_paypay_chart.sh
+++ ana1_paypay_chart.sh
@@ -2,12 +2,21 @@
 # 環境設定を読み込む
 source ./env.sh
 
+# 第1引数から最小割合を取得。指定がない場合はデフォルト値1.05を設定
+min_wari=${1:-1.05}
+
+# 引数が数値（整数または小数）であるかチェック
+if [[ ! "$min_wari" =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
+    echo "割合は数字で指定してください" >&2
+    exit 1
+fi
+
 # sqliteのdbファイルのパスは、環境変数のsqlite_workを参照
 my_db=$sqlite_work
 
 # brandテーブルからpaypayカラムに'P'が登録されているbrand_codeを抽出
 brand_codes=$(sqlite3 $db_opt_cmd $sqlite_work <<EOF
-.parameter set :min_wari 1.05
+.parameter set :min_wari $min_wari
 .read dml/paypay_001.sql
 EOF
 )
